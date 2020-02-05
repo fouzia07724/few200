3a3f99062fc6968c60820cdc130cbe55286d2c94
fouzia07724 <59837518+fouzia07724@users.noreply.github.com>
2020-02-05 16:32:44 +0000

    In Progress

--- src/app/features/booklist/components/list/list.component.ts
+++ src/app/features/booklist/components/list/list.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { BooklistItemModel } from '../../model';
+
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListComponent implements OnInit {
 
-  model: BooklistItemModel[] = [
-    { id: '1', description: 'Bear' },
-    { id: '2', description: 'More Bear' }
-  ];
+  // model: BooklistItemModel[] = [
+  //   { id: '1', description: 'Bear' },
+  //  { id: '2', description: 'More Bear' }
+  // ];
+  @Input() model: BooklistItemModel[];
 
   constructor() { }
 
--- src/app/features/shopping/components/list/list.component.ts
+++ src/app/features/shopping/components/list/list.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 import { ShoppingItemModel } from '../../model';
+
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListComponent implements OnInit {
 
-  model: ShoppingItemModel[] = [
-    { id: '1', description: 'Bear' },
-    { id: '2', description: 'More Bear' }
-  ];
+  // model: ShoppingItemModel[] = [
+  //   { id: '1', description: 'Bear' },
+  //   { id: '2', description: 'More Bear' }
+  // ];
 
+  @Input() model: ShoppingItemModel[];
   constructor() { }
 
   ngOnInit() {
